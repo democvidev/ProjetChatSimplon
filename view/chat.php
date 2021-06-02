5941879bb28cfe60cdc50221b3fdffc200e30304
democvidev <cnvvc_vlad@mail.ru>
2021-06-02 11:44:49 +0200

    updated chat

--- view/chat.php
+++ view/chat.php
@@ -1,17 +1,10 @@
 <table class="table table-hover">
-  
+  <?php for ($i = 0; $i < 5; $i++) { ?>
     <tr class="table-light">
       <th scope="row"></th>
       <td>02/06/2021</td>
       <td>Vladislav</td>
       <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptatem sunt at sequi molestiae sapiente officiis a, repellendus odit quia impedit in cupiditate mollitia totam? Libero blanditiis repudiandae dolorem dignissimos.</td>
-    </tr>
-    <tr class="table-light">
-      <th scope="row"></th>
-      <td>02/06/2021</td>
-      <td>Vladislav</td>
-      <td>Bonjour</td>
-    </tr>
-   
-  
+    </tr> 
+  <?php } ?>
 </table>
