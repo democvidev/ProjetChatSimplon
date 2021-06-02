3e15c5be9c2cb0434914a2e7f66501303ec33f93
democvidev <cnvvc_vlad@mail.ru>
2021-06-02 10:56:55 +0200

    dispatched files

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php
+
+require 'view/home.php';
+require 'view/chat.php';
+require 'view/form.php';
---
+++ view/chat.php
@@ -0,0 +1,17 @@
+<table class="table table-hover">
+  
+    <tr class="table-light">
+      <th scope="row"></th>
+      <td>02/06/2021</td>
+      <td>Vladislav</td>
+      <td>Bonjour</td>
+    </tr>
+    <tr class="table-light">
+      <th scope="row"></th>
+      <td>02/06/2021</td>
+      <td>Vladislav</td>
+      <td>Bonjour</td>
+    </tr>
+   
+  
+</table>
