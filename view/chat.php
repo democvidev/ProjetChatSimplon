b9bdc31c7ad7a483ec52bebf7ff47b578b1562f4
democvidev <cnvvc_vlad@mail.ru>
2021-06-04 14:06:34 +0200

    remove htmlspecialchars from date

--- view/chat.php
+++ view/chat.php
@@ -12,7 +12,7 @@
          foreach ($tab as $row) {
              ?>
     <tr class="row table-light">
-      <td class="col-2"><?= htmlspecialchars($row['date']) ?></td>
+      <td class="col-2"><?= $row['date'] ?></td>
       <td class="col-2"><?= htmlspecialchars($row['author']) ?></td>
       <td class="col-7"><?= nl2br(htmlspecialchars($row['content'])) ?></td>
       <td class="col-1"><a class="btn-warning" href="?id=<?= $row['id']?>&amp;action=update"><i class="bi bi-arrow-counterclockwise"></i></a> - <a class="btn-danger" href="?id=<?= $row['id'] ?>&amp;action=delete"><i class="bi bi-trash"></i></a></td>
