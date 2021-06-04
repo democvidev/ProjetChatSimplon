ebafae62247373bb752e685fd5b1011a6b8af1a1
democvidev <cnvvc_vlad@mail.ru>
2021-06-04 14:04:22 +0200

    updated security

--- view/chat.php
+++ view/chat.php
@@ -14,7 +14,7 @@
     <tr class="row table-light">
       <td class="col-2"><?= htmlspecialchars($row['date']) ?></td>
       <td class="col-2"><?= htmlspecialchars($row['author']) ?></td>
-      <td class="col-7"><?= htmlspecialchars($row['content']) ?></td>
+      <td class="col-7"><?= nl2br(htmlspecialchars($row['content'])) ?></td>
       <td class="col-1"><a class="btn-warning" href="?id=<?= $row['id']?>&amp;action=update"><i class="bi bi-arrow-counterclockwise"></i></a> - <a class="btn-danger" href="?id=<?= $row['id'] ?>&amp;action=delete"><i class="bi bi-trash"></i></a></td>
 
     </tr> 
